935af13549632da3d74d16a68a93c39f45895fcc
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-13 15:42:59 +0200

    Dynamic Components

--- vueschool-course/Dynamic Forms with Vue.js/dynamic-forms/src/components/FormWizard.vue
+++ vueschool-course/Dynamic Forms with Vue.js/dynamic-forms/src/components/FormWizard.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep"/>
-    <FormUserDetails v-if="currentStepNumber === 2" @update="processStep"/>
-    <FormAddress v-if="currentStepNumber === 3" @update="processStep" :wizard-data="form"/>
-    <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep" :wizard-data="form"/>
+    <component :is="currentStep" @update="processStep" :wizard-data="form"></component>
+   
 
     <div class="progress-bar">
       <div :style="`width: ${progress}%;`"></div>
@@ -45,7 +43,12 @@ export default {
     return {
       currentStepNumber: 1,
       canGoNext: false,
-      length: 4,
+      steps: [
+        'FormPlanPicker',
+    'FormUserDetails',
+    'FormAddress',
+    'FormReviewOrder'
+      ],
       form: {
         plan: null,
         email: null,
@@ -59,6 +62,12 @@ export default {
     }
   },
   computed: {
+    length () {
+      return this.steps.length
+    },
+    currentStep () {
+      return this.steps[this.currentStepNumber - 1]
+    },
     progress () {
       return this.currentStepNumber/this.length * 100
     }
