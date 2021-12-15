28377c133933f641af058963c7967541d2466bd4
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-15 13:02:01 +0200

    Vue.js Watchers and the Activated Hook

--- vueschool-course/Dynamic Forms with Vue.js/dynamic-forms/src/components/FormAddress.vue
+++ vueschool-course/Dynamic Forms with Vue.js/dynamic-forms/src/components/FormAddress.vue
@@ -9,13 +9,27 @@
     <form @input="submit" class="form">
       <div class="form-group">
         <label class="form-label" for="delivery_name">Name</label>
-        <input v-model="$v.form.recipient.$model" type="text" placeholder="Recipients Name" class="form-control" id="delivery_name">
-        <div v-if="$v.form.recipient.$error" class="error">field is required</div>
+        <input
+          v-model="$v.form.recipient.$model"
+          type="text"
+          placeholder="Recipients Name"
+          class="form-control"
+          id="delivery_name"
+        />
+        <div v-if="$v.form.recipient.$error" class="error">
+          field is required
+        </div>
       </div>
 
       <div class="form-group">
         <label class="form-label" for="address">Address</label>
-        <textarea v-model="$v.form.address.$model" placeholder="London Street 470978 New England" rows="3" class="form-control" id="address"></textarea>
+        <textarea
+          v-model="$v.form.address.$model"
+          placeholder="London Street 470978 New England"
+          rows="3"
+          class="form-control"
+          id="address"
+        ></textarea>
         <div v-if="$v.form.address.$error" class="error">field is required</div>
       </div>
     </form>
@@ -23,46 +37,48 @@
 </template>
 
 <script>
-  import {required} from 'vuelidate/lib/validators'
-  export default {
-    props: {
-      wizardData: {
-        type: Object,
-        required: true
-      }
+import { required } from "vuelidate/lib/validators";
+export default {
+  props: {
+    wizardData: {
+      type: Object,
+      required: true,
     },
-    data () {
-      return {
-        form: {
-          address: null,
-          recipient: this.wizardData.name
-        }
-      }
-    },
-    validations: {
+  },
+  data() {
+    return {
       form: {
-        address: {
-          required
-        },
-        recipient: {
-          required
-        }
-      }
+        address: null,
+        recipient: this.wizardData.name,
+      },
+    };
+  },
+  activated() {
+    this.form.recipient = this.wizardData.name;
+  },
+  validations: {
+    form: {
+      address: {
+        required,
+      },
+      recipient: {
+        required,
+      },
     },
-    methods: {
-      submit() {
-        this.$emit('update', {
-          data: {
-            recipient: this.form.recipient,
-            address: this.form.address
-          },
-          valid: !this.$v.$invalid
-        })
-        }
-    }
-  }
+  },
+  methods: {
+    submit() {
+      this.$emit("update", {
+        data: {
+          recipient: this.form.recipient,
+          address: this.form.address,
+        },
+        valid: !this.$v.$invalid,
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
--- vueschool-course/Dynamic Forms with Vue.js/dynamic-forms/src/components/FormWizard.vue
+++ vueschool-course/Dynamic Forms with Vue.js/dynamic-forms/src/components/FormWizard.vue
@@ -1,138 +1,136 @@
 <template>
   <div>
     <div v-if="wizardInProgress" v-show="asyncState !== 'pending'">
-    <keep-alive>
+      <keep-alive>
+        <component
+          ref="currentStep"
+          :is="currentStep"
+          @update="processStep"
+          :wizard-data="form"
+        ></component>
+      </keep-alive>
 
-    <component ref="currentStep" :is="currentStep" @update="processStep" :wizard-data="form"></component>
-    </keep-alive>
-   
+      <div class="progress-bar">
+        <div :style="`width: ${progress}%;`"></div>
+      </div>
 
-    <div class="progress-bar">
-      <div :style="`width: ${progress}%;`"></div>
+      <!-- Actions -->
+      <div class="buttons">
+        <button
+          @click="goBack"
+          v-if="currentStepNumber > 1"
+          class="btn-outlined"
+        >
+          Back
+        </button>
+        <button @click="nextButtonAction" :disabled="!canGoNext" class="btn">
+          {{ isLastStep ? "Complete Order" : "Next" }}
+        </button>
+      </div>
     </div>
-
-    <!-- Actions -->
-    <div class="buttons">
-      <button
-        @click="goBack"
-        v-if="currentStepNumber > 1"
-        class="btn-outlined"
-      >Back
-      </button>
-      <button
-        @click="nextButtonAction"
-        :disabled="!canGoNext"
-        class="btn"
-      >{{isLastStep ? 'Complete Order' : 'Next'}}</button>
-    </div>
-  </div>
-   <div v-else>
+    <div v-else>
       <h1 class="title">Thank you!</h1>
-      <h2 class="subtitle">
-        We look forward to shipping you your first box!
-      </h2>
+      <h2 class="subtitle">We look forward to shipping you your first box!</h2>
       <p class="text-center">
-        <a href="https://vueschool.io" target="_blank" class="btn">Go somewhere cool!</a>
+        <a href="https://vueschool.io" target="_blank" class="btn"
+          >Go somewhere cool!</a
+        >
       </p>
     </div>
     <div class="loading-wrapper" v-if="asyncState === 'pending'">
       <div class="loader">
-        <img src="/spinner.svg" alt="">
+        <img src="/spinner.svg" alt="" />
         <p>Please wait, we're hitting our servers!</p>
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
-import { postFormToDB } from '../api'
-import FormPlanPicker from './FormPlanPicker'
-import FormUserDetails from './FormUserDetails'
-import FormAddress from './FormAddress'
-import FormReviewOrder from './FormReviewOrder'
+import { postFormToDB } from "../api";
+import FormPlanPicker from "./FormPlanPicker";
+import FormUserDetails from "./FormUserDetails";
+import FormAddress from "./FormAddress";
+import FormReviewOrder from "./FormReviewOrder";
 export default {
-  name: 'FormWizard',
+  name: "FormWizard",
   components: {
     FormPlanPicker,
     FormUserDetails,
     FormAddress,
-    FormReviewOrder
+    FormReviewOrder,
   },
-  data () {
+  data() {
     return {
       currentStepNumber: 1,
       canGoNext: false,
       asyncState: null,
       steps: [
-        'FormPlanPicker',
-    'FormUserDetails',
-    'FormAddress',
-    'FormReviewOrder'
+        "FormPlanPicker",
+        "FormUserDetails",
+        "FormAddress",
+        "FormReviewOrder",
       ],
       form: {
         plan: null,
         email: null,
         name: null,
         password: null,
-        address: null, 
+        address: null,
         recipient: null,
         chocolate: false,
-        otherTreat: false
-      }
-    }
+        otherTreat: false,
+      },
+    };
   },
   computed: {
     isLastStep() {
-      return this.currentStepNumber === this.length
+      return this.currentStepNumber === this.length;
     },
-    wizardInProgress () {
-      return this.currentStepNumber <= this.length
-
+    wizardInProgress() {
+      return this.currentStepNumber <= this.length;
     },
-    length () {
-      return this.steps.length
+    length() {
+      return this.steps.length;
     },
-    currentStep () {
-      return this.steps[this.currentStepNumber - 1]
+    currentStep() {
+      return this.steps[this.currentStepNumber - 1];
     },
-    progress () {
-      return this.currentStepNumber/this.length * 100
-    }
+    progress() {
+      return (this.currentStepNumber / this.length) * 100;
+    },
   },
   methods: {
-    sumbitOrder () {
-      this.asyncState = 'pending'
-      postFormToDB(this.form)
-      .then(()=> {
-        console.log('form sumbitted', this.form);
-        this.asyncState = 'success'
-        this.currentStepNumber++
-      })
+    sumbitOrder() {
+      this.asyncState = "pending";
+      postFormToDB(this.form).then(() => {
+        console.log("form sumbitted", this.form);
+        this.asyncState = "success";
+        this.currentStepNumber++;
+      });
     },
-    nextButtonAction () {
+    nextButtonAction() {
       if (this.isLastStep) {
-        this.sumbitOrder()
+        this.sumbitOrder();
       } else {
-        this.goNext()
+        this.goNext();
       }
-    }, 
-    processStep (step) {
-      Object.assign(this.form, step.data)
-      this.canGoNext = step.valid
     },
-    goBack () {
-      this.currentStepNumber--
-      this.canGoNext = true 
-
+    processStep(step) {
+      Object.assign(this.form, step.data);
+      this.canGoNext = step.valid;
     },
-    goNext () {
-      this.currentStepNumber++
+    goBack() {
+      this.currentStepNumber--;
+      this.canGoNext = true;
+    },
+    goNext() {
+      this.currentStepNumber++;
       // this.canGoNext = false
       this.$nextTick(() => {
-        this.canGoNext = !this.$refs.currentStep.$v.$invalid
-      })
-    }
-  }
-}
+        this.canGoNext = !this.$refs.currentStep.$v.$invalid;
+      });
+    },
+  },
+};
 </script>
