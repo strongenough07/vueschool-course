08bab5aa465f8e3bcf7f74ee3030d42a3c7dd42f
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-21 17:47:27 +0200

    How to structure complex render functions

--- vueschool-course/Functional Components/functional-components/src/App.vue
+++ vueschool-course/Functional Components/functional-components/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <UserAvatar />
-
-    <!-- <h1>User Card</h1>
+    <h1>User Card</h1>
     <div class="center">
       <UserCard
         :user="user"
-
-          ><strong>{{ user.bio }}</strong></template
-        >
+        @click.native="nameAlert(user.name)"
+        @contact="contactUser(user.name)"
+      >
+        <strong>{{ user.bio }}</strong>
       </UserCard>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-// import UserCard from './components/user-card';
-import UserAvatar from "./components/user-avatar.vue";
+import UserCard from "./components/user-card.vue";
 export default {
-  name: "App",
-
   components: {
-    // UserCard,
-    UserAvatar,
+    UserCard,
   },
 };
 </script>
