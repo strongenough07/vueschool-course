173d22df4b997a9227bcbae99f1d4d757546dbe9
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-21 17:41:27 +0200

    Nesting render functions

--- vueschool-course/Functional Components/functional-components/src/App.vue
+++ vueschool-course/Functional Components/functional-components/src/App.vue
@@ -1,17 +1,6 @@
 <template>
   <div id="app">
-    <ListWrapper listType="ul">
-      <li>Wonder Woman</li>
-      <li>Batman</li>
-      <li>Superman</li>
-      <li>Aquaman</li>
-    </ListWrapper>
-    <ListWrapper listType="ol">
-      <li>Wonder Woman</li>
-      <li>Batman</li>
-      <li>Superman</li>
-      <li>Aquaman</li>
-    </ListWrapper>
+    <UserAvatar />
 
     <!-- <h1>User Card</h1>
     <div class="center">
@@ -27,13 +16,13 @@
 
 <script>
 // import UserCard from './components/user-card';
-import ListWrapper from "./components/list-wrapper.vue";
+import UserAvatar from "./components/user-avatar.vue";
 export default {
   name: "App",
 
   components: {
     // UserCard,
-    ListWrapper,
+    UserAvatar,
   },
 };
 </script>
---
+++ vueschool-course/Functional Components/functional-components/src/components/user-avatar.vue
@@ -0,0 +1,21 @@
+
+
+<script>
+export default {
+  functional: true,
+  name: "UserAvatar",
+
+  render(h) {
+    return h("div", { attrs: { class: "avatar" } }, [
+      h("figure", [
+        h("img", {
+          attrs: {
+            src: "/avatar.png",
+            alt: "Jane Doe",
+          },
+        }),
+      ]),
+    ]);
+  },
+};
+</script>
