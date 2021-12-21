9a2752e154f6c47cc5e9d6af9e78a1f00df4d788
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-21 16:26:50 +0200

    Introduction to render functions

--- vueschool-course/Functional Components/functional-components/src/App.vue
+++ vueschool-course/Functional Components/functional-components/src/App.vue
@@ -1,46 +1,39 @@
 <template>
   <div id="app">
-    <h1>User Card</h1>
+    <ListWrapper listType="ul">
+      <li>Wonder Woman</li>
+      <li>Batman</li>
+      <li>Superman</li>
+      <li>Aquaman</li>
+    </ListWrapper>
+    <ListWrapper listType="ol">
+      <li>Wonder Woman</li>
+      <li>Batman</li>
+      <li>Superman</li>
+      <li>Aquaman</li>
+    </ListWrapper>
+
+    <!-- <h1>User Card</h1>
     <div class="center">
       <UserCard
         :user="user"
-        @click.native="nameAlert(user.name)"
-        @contact="contactUser(user.name)"
-      >
-        <template #body>
-          <strong>
-            {{ user.bio }}
-          </strong>
-        </template>
+
+          ><strong>{{ user.bio }}</strong></template
+        >
       </UserCard>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-import UserCard from "./components/user-card";
+// import UserCard from './components/user-card';
+import ListWrapper from "./components/list-wrapper.vue";
 export default {
   name: "App",
-  data() {
-    return {
-      user: {
-        name: "Jane Doe",
-        role: "Staff Engineer",
-        avatar: "https://randomuser.me/api/portraits/women/26.jpg",
-        bio: "Candy canes sweet roll pudding liquorice jelly beans brownie powder pie. Pudding powder marshmallow sugar plum dessert pastry jelly-o pastry marzipan. Jelly topping biscuit jelly beans cookie cheesecake.",
-      },
-    };
-  },
-  methods: {
-    nameAlert(name) {
-      alert(`Clicked on ${name}`);
-    },
-    contactUser(name) {
-      alert(`Contacting ${name}`);
-    },
-  },
+
   components: {
-    UserCard,
+    // UserCard,
+    ListWrapper,
   },
 };
 </script>
---
+++ vueschool-course/Functional Components/functional-components/src/components/list-wrapper.vue
@@ -0,0 +1,12 @@
+
+<script>
+export default {
+  name: "ListWrapper",
+
+  functional: true,
+
+  render(h, context) {
+    return h(context.props.listType, context.chilndren);
+  },
+};
+</script>
