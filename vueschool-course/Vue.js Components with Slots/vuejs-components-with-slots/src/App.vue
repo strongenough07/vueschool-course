be36ff911a0bf4b0aa0c32dbe32d52814f000d24
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-23 11:59:55 +0200

    Named Slots

--- vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/App.vue
+++ vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    <AppButton>press me</AppButton>
-    <AppButton>like me</AppButton>
-    <AppButton>buy me</AppButton>
-  </div>
+  <AppLayout>
+    <template #header>Slots are awesome</template>
+    <template #default>
+      <AppButton @click="log">
+        <AppIcon name="heart" />
+        add to favorites
+      </AppButton>
+    </template>
+  </AppLayout>
 </template>
 
 <script>
+import AppLayout from "@/components/AppLayout";
 import AppButton from "@/components/AppButton";
-
+import AppIcon from "@/components/AppIcon";
 export default {
   components: {
     AppButton,
+    AppIcon,
+    AppLayout,
+  },
+  methods: {
+    log() {
+      console.log("works");
+    },
   },
 };
 </script>
+
+<style>
+</style>
--- vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/main.js
+++ vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import App from "./views/Lessons/1/index.vue";
+import App from "./App.vue";
 import "@/assets/index.css";
 
 Vue.config.productionTip = false;
