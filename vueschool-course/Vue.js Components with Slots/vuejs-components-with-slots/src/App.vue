d5acff493264dd65d701092d7f6d1ace564e2a86
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-23 10:28:29 +0200

    init

---
+++ vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AppButton>press me</AppButton>
+    <AppButton>like me</AppButton>
+    <AppButton>buy me</AppButton>
+  </div>
+</template>
+
+<script>
+import AppButton from "@/components/AppButton";
+
+export default {
+  components: {
+    AppButton
+  }
+};
+</script>
