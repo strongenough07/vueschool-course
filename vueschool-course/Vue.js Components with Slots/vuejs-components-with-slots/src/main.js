c08b0bbf8b5817a94dd98181647934a69fbfc96d
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-23 11:25:13 +0200

    Using Slots

--- vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/main.js
+++ vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import '@/assets/index.css'
+import Vue from "vue";
+import App from "./views/Lessons/1/index.vue";
+import "@/assets/index.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ vueschool-course/Vue.js Components with Slots/vuejs-components-with-slots/src/views/Lessons/1/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <AppButton @click="log">
+      add to favorites
+      <Icon />
+    </AppButton>
+  </div>
+</template>
+
+<script>
+import Icon from "./components/Icon.vue";
+import AppButton from "./components/AppButton.vue";
+
+export default {
+  components: {
+    AppButton,
+    Icon,
+  },
+  methods: {
+    log() {
+      console.log("works");
+    },
+  },
+};
+</script>
