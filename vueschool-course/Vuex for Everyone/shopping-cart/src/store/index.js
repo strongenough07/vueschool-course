44dc957d832cfe32f2e76903106d803b531355dd
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2021-12-24 16:32:18 +0200

    Add products to the cart

--- vueschool-course/Vuex for Everyone/shopping-cart/src/store/index.js
+++ vueschool-course/Vuex for Everyone/shopping-cart/src/store/index.js
@@ -7,7 +7,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     // = data
-    products: []
+    products: [],
+    cart: []
   },
 
   getters: {
@@ -27,12 +28,37 @@ export default new Vuex.Store({
           resolve();
         });
       });
+    },
+    addProductToCart(context, product) {
+      if (product.inventory > 0) {
+        const cartItem = context.state.cart.find(
+          item => item.id === product.id
+        );
+        if (!cartItem) {
+          context.commit("pushProductToCart", product.id);
+        } else {
+          context.commit("incrementItemQuantity", cartItem);
+        }
+        context.commit("decrementProductInventory", product);
+      }
     }
   },
 
   mutations: {
     setProducts(state, products) {
       state.products = products;
+    },
+    pushProductToCart(state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      });
+    },
+    incrementItemQuantity(state, cartItem) {
+      cartItem.quantity++;
+    },
+    decrementProductInventory(state, product) {
+      product.inventory--;
     }
   }
 });
